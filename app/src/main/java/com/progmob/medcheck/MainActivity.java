57af6ae85acd5c05bcc1283d542bdc6586de2840
Dekin <indrayanakadek@gmail.com>
2019-11-25 16:00:51 +0800

    dekin part

--- app/src/main/java/com/progmob/medcheck/MainActivity.java
+++ app/src/main/java/com/progmob/medcheck/MainActivity.java
@@ -2,7 +2,11 @@ package com.progmob.medcheck;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.ImageFormat;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView ivPasien = (ImageView) findViewById(R.id.iv_pasien);
+
+        ivPasien.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, PasienActivity.class);
+                        startActivity(intent);
+            }
+        });
+
     }
+
+
+
 }
