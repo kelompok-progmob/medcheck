cf26555861db5acbd81b2452fbb7b26cf7c578e4
Dekin <indrayanakadek@gmail.com>
2019-11-25 15:48:27 +0800

    dekin part

---
+++ app/src/main/java/com/progmob/medcheck/PasienActivity.java
@@ -0,0 +1,47 @@
+package com.progmob.medcheck;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class PasienActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pasien);
+
+        loadFragment(new PasienFragment());
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bn_main);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+        switch (item.getItemId()){
+            case R.id.pasien_menu:
+                fragment = new PasienFragment();
+                break;
+            case R.id.history_menu:
+                fragment = new HistoryFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fl_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+}
