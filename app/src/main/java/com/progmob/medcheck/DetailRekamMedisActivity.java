3384655676a35cb7dc437dfac32e40ba88d5506e
adityaherlambang <herlambangadt@gmail.com>
2019-12-09 16:05:00 +0800

    Update detail rekam medis

--- app/src/main/java/com/progmob/medcheck/DetailRekamMedisActivity.java
+++ app/src/main/java/com/progmob/medcheck/DetailRekamMedisActivity.java
@@ -11,12 +11,26 @@ public class DetailRekamMedisActivity extends AppCompatActivity {
 
     ActivityDetailRekamMedisBinding binding;
 
+    String namaPasien, keluhan;
+    int idPasien;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(DetailRekamMedisActivity.this, R.layout.activity_detail_rekam_medis);
 
+        Bundle extras = getIntent().getExtras();
+        namaPasien = extras.getString("nama_pasien");
+        keluhan = extras.getString("keluhan");
+        idPasien = extras.getInt("id_pasien");
 
+        setComponents();
 
     }
+
+    private void setComponents(){
+        binding.pasien.setText(namaPasien);
+        binding.keluhan.setText(keluhan);
+    }
+
 }
